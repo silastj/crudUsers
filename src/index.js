0e9f205a9978792bad009d5653c0b1a5a6f97fa0
Amós Silas <silastj@hotmail.com>
2023-04-02 18:38:14 -0300

    comment sequelizerc

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const app = express()
 
-require('./database')
+// require('./database')
 app.use(express.json())
 app.use(routes)
 const PORT = 3030

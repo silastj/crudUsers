b7aa8f05dd8301acd85947ad0928eed4dc1d0fb4
Amós Silas <silastj@hotmail.com>
2023-04-03 00:12:19 -0300

    add props

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,12 @@
 module.exports = {
   dialect: "mysql",
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
-  host: process.env.DB_HOST,
-  port:process.env.DB_PORT
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "6eKEpZrsxmssXlqsgGvd",
+  database: process.env.DB_DBNAME || "railway",
+  host: process.env.DB_HOST || "containers-us-west-171.railway.app",
+  port:process.env.DB_PORT || "6298",
+  define: {
+    timestamp: true,
+    underscored:true
+  }
 }

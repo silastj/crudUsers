cc716964614a7d20351018252db4c19166ac4d4a
Amós Silas <silastj@hotmail.com>
2023-04-02 23:53:54 -0300

    add port

--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,6 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
-  host: process.env.DB_HOST
+  host: process.env.DB_HOST,
+  port:process.env.DB_PORT
 }

ecbf6575696ed406f8cdaaeec473bb5e5b7f6ca4
Amós Silas <silastj@hotmail.com>
2023-12-16 15:42:59 -0300

    dynamic bd

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: "postgres",
-  username: "postgres",
-  password: "1234",
-  database: "teste",
-  host: "localhost",
-  port: "5432",
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_DBNAME || "teste",
+  host: process.env.DB_HOST || "localhost",
+  port:process.env.DB_PORT || "5432",
   define: {
     timestamp: true,
     underscored:true
